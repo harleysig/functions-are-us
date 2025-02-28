82a66b3aa740fbf75a4071a33a5399f7c6f610ab
Paris-Jane <parisjan@byu.edu>
2025-02-28 12:05:14 -0700

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -1,9 +1,6 @@
-
-#1. Display an introduction to the game explaining rules and prompt for their name and display that in the welcome message.
-#  Return the name to the main program and store it in variable so it can be used throughout the program.
 import random
 
-def welcome(): 
+def welcome() : 
     # Welcome user
     print( "Welcome to the game simulator")
     # Get their name
@@ -12,11 +9,11 @@ def welcome():
     print("\nHello " + sName + ", here are the instructions for the game simulator.\nFirst select your team. Then select the teams you want to play against.\nYou can play agaisnt as many teams as you'd like.\n")
     return sName
 
-def menu():
+def menu() :
     while True: 
         print("""
-        1. Start Over
-        2. Select Home Team
+        1. Select Team & Play Game
+        2. Search Records
         3. Quit
         """)
         choice = input("Select an option: ")
@@ -24,59 +21,59 @@ def menu():
             print("Please enter 1, 2, or 3")
         else:
             return choice
-          
-# set up list of teams, setup selection of home and opp teams
-def teamSelect() :
+
+
+def team(t=1,home_team="BYU") :
     teams = ["BYU", "ASU", "USU", "UofU", "BSU", "WSU", "WVU"]
+    if t == "op":
+        teams.remove(home_team)
+
     index = 1
 
     for item in teams:
         print (f"{index}. {item}")
-        index = index + 1
+        index += 1
 
-    selection = int(input("Select your home team number:\n"))
-    homeTeam = teams[selection-1]
-    teams.remove(homeTeam)
-    print (homeTeam)
+    choiceTeam = int(input("\nWhich team would you like to select (choose a number)? "))-1
+    return teams[choiceTeam]
 
+def play(home_team,away_team,team_dict) :
+    homeScore = random.randint(0,10)
+    opScore = random.randint(0,10)
+    print(f'\nGame Results: {home_team} {homeScore} - {away_team} {opScore}')
+    if homeScore > opScore : 
+        print(f'{home_team} wins!\n')
+        team_dict[home_team].append("W")
+        team_dict[away_team].append("L")
+    else : 
+        print(f'{away_team} wins!\n')
+        team_dict[home_team].append("L")
+        team_dict[away_team].append("W")
 
-def calc_score(home_team, away_team):
-    home_score = 0
-    opp_score = 0
-    while home_score == opp_score:
-        home_score = random.randrange(0,10)
-        opp_score = random.randrange(0,10)
+def record(team_dict) : 
+    teamChoice = input("\nEnter Team Name: ").upper()
+    if teamChoice in team_dict : 
+        print(f'\n{teamChoice} Season Results: {team_dict[teamChoice].count("W")} Wins - {team_dict[teamChoice].count("L")} Losses.\n')
+    else : 
+        print(f'\n{teamChoice} is not in the list. Please try again.\n')
 
-    if home_score > opp_score:
-        outcome = "W"
-    else:
-        outcome = "L"
-    
-    print(f"{home_team} scored {home_score}. {away_team} scored {opp_score}.")
-    return outcome
-
-
-# Assign their name to a vairiable
-
-teamSelect()
-
-
-def main() : #Main function to call all other functions
-    sPlayerName = welcome()
-    teams = {}
+def main() : 
+    playName = welcome()
+    team_dict = {"BYU" : [], "ASU" : [], "USU" : [], "UofU" : [], "BSU" : [], "WSU" : [], "WVU" : []}
 
     while True: 
         choice = menu()
         if choice == "1" : 
-          print("placeholder")
+            home_team = team()
+            away_team = team("op",home_team)
+            play(home_team,away_team,team_dict)
         elif choice == "2" : 
-            home_team=teamSelect()
-            away_team=teamSelect()
-            calc_score(home_team,away_team)
+            record(team_dict)
         elif choice == "3" : 
-            print(f'Thanks for playing! Goodbye {sPlayerName.capitalize()}!')
+            print(f'Thanks for playing! Goodbye {playName.capitalize()}!')
             break
         else :
             print(f'Invalid entry. Please try again.')
+
 
 main()
