58d2c4d818046e8882e658113604818e2e270e1b
robisonwill <will.robison2203@gmail.com>
2025-02-28 10:58:04 -0700

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -8,10 +8,28 @@ def Welcome():
     # Get their name
     sName = input("What is your name? ")
     # Display instructions using their name
-    print("\n" + sName + " here are the instructions for the game simulator.\nFirst select your team. Then select the teams you want to play against.\nYou can play agaisnt as many teams as you'd like.\n")
+    print("\nHello " + sName + ", here are the instructions for the game simulator.\nFirst select your team. Then select the teams you want to play against.\nYou can play agaisnt as many teams as you'd like.\n")
     return sName
- 
+
+# set up list of teams, setup selection of home and opp teams
+def teamSelect() :
+    teams = ["BYU", "ASU", "USU", "UofU", "BSU", "WSU", "WVU"]
+    index = 1
+
+    for item in teams:
+        print (f"{index}. {item}")
+        index = index + 1
+
+    selection = int(input("Select your home team number:\n"))
+    homeTeam = teams[selection-1]
+    teams.remove(homeTeam)
+    print (homeTeam)
+
+
+
 # Assign their name to a vairiable
 sPlayerName = Welcome()
+
+teamSelect()
 
 
