1d7be12f8566e5d2cdaa9b794a5f06526c11281b
Paris-Jane <parisjan@byu.edu>
2025-02-28 10:42:12 -0700

    add base main function

--- introduction.py
+++ introduction.py
@@ -2,7 +2,7 @@
 #1. Display an introduction to the game explaining rules and prompt for their name and display that in the welcome message.
 #  Return the name to the main program and store it in variable so it can be used throughout the program.
 
-def Welcome(): 
+def welcome(): 
     # Welcome user
     print( "Welcome to the game simulator")
     # Get their name
@@ -10,7 +10,7 @@ def Welcome():
     # Display instructions using their name
     print("\n" + sName + " here are the instructions for the game simulator.\nFirst select your team. Then select the teams you want to play against.\nYou can play agaisnt as many teams as you'd like.\n")
     return sName
- 
+
 def menu():
     while True: 
         print("""
@@ -25,6 +25,19 @@ def menu():
             return choice
 
 # Assign their name to a vairiable
-sPlayerName = Welcome()
-choice = menu()
 
+
+def main() : #Main function to call all other functions
+    sPlayerName = welcome()
+    teams = {}
+
+    while True: 
+        choice = menu()
+        if choice == "1" : 
+        elif choice == "2" : 
+        elif choice == "3" : 
+            print(f'Thanks for playing! Goodbye {sPlayerName.capitalize()}!')
+        else :
+            print(f'Invalid entry. Please try again.')
+
+main()
